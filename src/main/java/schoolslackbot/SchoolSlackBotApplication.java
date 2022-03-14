dbc2e4a450fc7f01cf24743dbdfa28a02f2ffd1b
SonYoonSeok <dbstjr5517@naver.com>
2022-03-14 11:26:23 +0900

    Initial commit

---
+++ src/main/java/schoolslackbot/SchoolSlackBotApplication.java
@@ -0,0 +1,13 @@
+package schoolslackbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSlackBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolSlackBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/schoolslackbot/SchoolSlackBotApplicationTests.java
@@ -0,0 +1,13 @@
+package schoolslackbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSlackBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
