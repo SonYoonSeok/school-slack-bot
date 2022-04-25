ae73a2b7863500c959847c8ae08d95206390d612
SonYoonSeok <dbstjr5517@naver.com>
2022-04-25 11:43:02 +0900

    refactor: 메세지 전송 메서드 비동기로 수정

--- src/main/java/schoolslackbot/service/SlackService.java
+++ src/main/java/schoolslackbot/service/SlackService.java
@@ -3,30 +3,39 @@ package schoolslackbot.service;
 import com.slack.api.Slack;
 import com.slack.api.webhook.Payload;
 import com.slack.api.webhook.WebhookResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 
+@Slf4j
 @Service
 public class SlackService {
 
     @Value("${url.webhook}")
     private String webhookUrl;
 
-    public WebhookResponse sendMessage(String message) {
-        WebhookResponse response = null;
+    @Async
+    public CompletableFuture<WebhookResponse> sendMessage(String message) {
+        CompletableFuture<WebhookResponse> future = new CompletableFuture<>();
         Slack slack = Slack.getInstance();
-
         try {
             Payload payload = Payload.builder()
                     .text(message)
                     .build();
-            response = slack.send(webhookUrl, payload);
+            final WebhookResponse response = slack.send(webhookUrl, payload);
+            future = CompletableFuture
+                    .supplyAsync(() -> {
+                        log.info("runAsync");
+                        return response;
+                    });
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return response;
+        return future;
     }
 }
